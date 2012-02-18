8645eedd5f37bf54e3e99dd5fc75928a11c17092
blueblank <blueblank@gmail.com>
2012-02-18 10:14:47 -0500

    refining timestamps check

--- lib/modbot/modbot_fetch.rb
+++ lib/modbot/modbot_fetch.rb
@@ -1,44 +1,43 @@
 module ModbotFetch
 
-    #Checks reported items for any matching conditions: report, spam, or submission
-    def fetch_results(which_q, subreddit)
-      which_to = self.method("get_reddit_#{which_q}s")
-      timestamp = get_timestamp(which_q, subreddit, which_to)
-      proceed = compare_timestamp(subreddit.timestamps["#{which_q}_last"], timestamp)
-      if proceed
-        results = which_to.call(subreddit.name, subreddit.item_limit) 
-        @l.info "#{results.count} results from #{which_q}"
-        results = filterby_timestamp(which_q, subreddit, results)
-        if results.empty?
-          subreddit["#{which_q}_recent"] = []
-          @l.info "nothing to report, #{which_q} is empty" 
-        else
-          #check_alerts("#{which_q}_threshold".to_sym, results.count, subreddit)
-          subreddit["#{which_q}_recent"] = results
-          @l.info "#{results.count} new items from #{which_q} to check"
-        end
+  #Checks reported items for any matching conditions: report, spam, or submission
+  def fetch_results(which_q, subreddit)
+    which_to = self.method("get_reddit_#{which_q}s")
+    timestamp = get_timestamp(which_q, subreddit, which_to)
+    proceed = compare_timestamp(subreddit.timestamps["#{which_q}_last"], timestamp)
+    if proceed
+      results = which_to.call(subreddit.name, subreddit.item_limit) 
+      @l.info "#{results.count} results from #{which_q}"
+      results = filterby_timestamp(which_q, subreddit, results)
+      if results.empty?
+        subreddit["#{which_q}_recent"] = []
+        @l.info "nothing to report, #{which_q} is empty" 
       else
-        @l.info "nothing new for #{which_q}"
+        #check_alerts("#{which_q}_threshold".to_sym, results.count, subreddit)
+        subreddit["#{which_q}_recent"] = results
+        @l.info "#{results.count} new items from #{which_q} to check"
       end
+    else
+      @l.info "nothing new for #{which_q}"
     end
+  end
 
-    #see if time has changed on newest item, filter for only items newer than last check
-    def filterby_timestamp(which_q, subreddit, results)
-      time_to_check = subreddit.timestamps["#{which_q}_last"]
-      results = results.select { |r| r.timestamp > time_to_check }
-      subreddit.timestamps["#{which_q}_last"] = results[0].timestamp
-        @l.info "#{which_q} results filtered against most recent time of check, new timestamp #{results[0].timestamp}"
-      end
-      results
+  #see if time has changed on newest item, filter for only items newer than last check
+  def filterby_timestamp(which_q, subreddit, results)
+    time_to_filter = subreddit.timestamps["#{which_q}_last"]
+    results = results.select { |r| r.timestamp > time_to_filter }
+    subreddit.timestamps["#{which_q}_last"] = results[0].timestamp
+    @l.info "#{which_q} results filtered against most recent time of check, new timestamp #{results[0].timestamp}"
+    results
+  end
+
+  def compare_timestamp(subreddit_timestamp, timestamp)
+    if subreddit_timestamp >= timestamp
+      return false
+    else
+      return true
     end
-
-   def compare_timestamp(subreddit_timestamp, timestamp)
-     if subreddit_timestamp >= timestamp
-       return false
-     else
-       return true
-     end
-   end
+  end
 
   def get_timestamp(which_q, subreddit, which_to)
     result = which_to.call(subreddit.name, 1)

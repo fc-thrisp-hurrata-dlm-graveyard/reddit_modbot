ea57007aba4a40b8cdbbc9961e801b2f4dc339c7
blueblank <blueblank@gmail.com>
2012-02-18 10:11:32 -0500

    refining timestamps check

--- lib/modbot/modbot_fetch.rb
+++ lib/modbot/modbot_fetch.rb
@@ -8,12 +8,12 @@ module ModbotFetch
       if proceed
         results = which_to.call(subreddit.name, subreddit.item_limit) 
         @l.info "#{results.count} results from #{which_q}"
-        results = compare_times(which_q, results)
+        results = filterby_timestamp(which_q, subreddit, results)
         if results.empty?
           subreddit["#{which_q}_recent"] = []
           @l.info "nothing to report, #{which_q} is empty" 
         else
-          check_alerts("#{which_q}_threshold".to_sym, results.count, subreddit)
+          #check_alerts("#{which_q}_threshold".to_sym, results.count, subreddit)
           subreddit["#{which_q}_recent"] = results
           @l.info "#{results.count} new items from #{which_q} to check"
         end
@@ -23,20 +23,10 @@ module ModbotFetch
     end
 
     #see if time has changed on newest item, filter for only items newer than last check
-    def compare_times(which_q, subreddit, results)
-      if subreddit.timestamps.send(which_q.to_sym).nil?
-        @l.info "this is the most recent set of results for #{which_q}"
-        if results.empty?
-           subreddit.timestamps["#{which_q}_last"] = Time.now.to_f.round(3)
-        else
-           subreddit.timestamps["#{which_q}_last"] = results[0].timestamp
-        end 
-        results = results
-      else
-        what_time = (which_q + '_last').to_sym
-        time_to_check = subreddit.timestamps.send(what_time)
-        results = results.select { |r| r.timestamp > time_to_check }
-        subreddit.timestamps[(which_q +'_last')] = results[0].timestamp
+    def filterby_timestamp(which_q, subreddit, results)
+      time_to_check = subreddit.timestamps["#{which_q}_last"]
+      results = results.select { |r| r.timestamp > time_to_check }
+      subreddit.timestamps["#{which_q}_last"] = results[0].timestamp
         @l.info "#{which_q} results filtered against most recent time of check, new timestamp #{results[0].timestamp}"
       end
       results

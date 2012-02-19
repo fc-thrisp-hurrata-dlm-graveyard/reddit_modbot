be78e79d660b067800d3d212f466b0c63a6add11
req <req@certain.(null)>
2012-02-19 17:51:27 -0500

    log only actually scoring items

--- lib/modbot/modbot_score.rb
+++ lib/modbot/modbot_score.rb
@@ -11,7 +11,10 @@ module ModbotScore
   def score_results(results_set)
     results_set.each do |item|
       item.score = score_verdict(item.verdict, :approve) / score_verdict(item.verdict, :remove)
-      @l.info "#{formatted_item_verdict(item.verdict)} yields a score of #{item.score} for item #{item.fullid}"
+      final = formatted_item_verdict(item.verdict)
+      if final.count > 0
+        @l.info "#{final} yields a score of #{item.score} for item #{item.fullid}"
+      end
     end
   end
 

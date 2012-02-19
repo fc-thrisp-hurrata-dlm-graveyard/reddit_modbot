00fb8de527bb221c37236d06ed2a57e9211a53bb
req <req@certain.(null)>
2012-02-19 11:43:45 -0500

    general fixes

--- lib/modbot/modbot_score.rb
+++ lib/modbot/modbot_score.rb
@@ -1,12 +1,12 @@
 module ModbotScore
 
-  def score(input, selection)
+  def score_verdict(input, selection)
     input.select { |x| x[0] == selection }.collect {|x,y| y}.inject(:+).to_f
   end
 
   def score_results(results_set)
     results_set.each do |item|
-      item.score = score(item.verdict, :approve) / score(item.verdict, :remove)
+      item.score = score_verdict(item.verdict, :approve) / score_verdict(item.verdict, :remove)
       @l.info "#{item.verdict} yields a score of #{item.score} for item #{item.fullid}"
     end
   end

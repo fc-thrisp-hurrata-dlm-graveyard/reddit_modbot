5d51eb13f044471577dd0ce45ed4945c1893baa5
qx <qx@surface.(null)>
2012-03-03 12:39:54 -0500

    revising retrieved routes for eventual commenting

--- lib/modbot/reddit_wrap.rb
+++ lib/modbot/reddit_wrap.rb
@@ -108,8 +108,8 @@ module RedditWrap
           h.id = yy['data']['id']
           h.fullid = yy['data']['name']
           h.permalink = provide_link(yy)#reddit_route(yy['data']['permalink'])
-          self.minimal_author ? h.author = yy['data']['author'] : reddit_user(yy['data']['author'])
-          #h.author = reddit_user(yy['data']['author'])
+          #self.minimal_author ? h.author = yy['data']['author'] : reddit_user(yy['data']['author'])
+          h.author = reddit_user(yy['data']['author'])
           if yy['kind'] == "t1"
             h.kind = "comment"
             h.comment = yy['data']['body']
